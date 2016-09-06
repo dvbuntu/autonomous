1e9b9f05e203d008165d38fb3351d8c5aa3b7249
Dan Van Boxel <the.dvb@gmail.com>
2016-09-06 19:23:35 -0400

    Better combined model
    Still training

--- complex.py
+++ complex.py
@@ -34,7 +34,7 @@ ncols = 64
 real_in = Input(shape=(2,), name='real_input')
 
 # video frame in, grayscale
-frame_in = Input(shape=(3,nrows,ncols))
+frame_in = Input(shape=(3,nrows,ncols), name='img_input')
 
 # convolution for image input
 conv1 = Convolution2D(4,5,5,border_mode='same', W_regularizer=l2(0.001))
@@ -52,42 +52,48 @@ conv_l3 = conv3(pool_l2)
 Econv_l3 = ELU()(conv_l3)
 pool_l3 = MaxPooling2D(pool_size=(2,2))(Econv_l3)
 
-flat = Flatten()(pool_l3)
+drop_l3 = Dropout(.5)(pool_l3)
+
+flat = Flatten()(drop_l3)
 
 M = merge([flat,real_in], mode='concat', concat_axis=1)
 
 D1 = Dense(64,W_regularizer=l2(0.001))(M)
 ED1 = ELU()(D1)
-D2 = Dense(32,W_regularizer=l2(0.001))(ED1)
+DED1 = Dropout(0.5)(ED1)
+D2 = Dense(32,W_regularizer=l2(0.001))(DED1)
 ED2 = ELU()(D2)
 D3 = Dense(32,W_regularizer=l2(0.001))(ED2)
 ED3 = ELU()(D3)
 
 S1 = Dense(32,W_regularizer=l2(0.001))(ED3)
 ES1 = ELU()(S1)
-S2 = Dense(32,W_regularizer=l2(0.001))(S1)
+DES1 = Dropout(0.5)(ES1)
+S2 = Dense(32,W_regularizer=l2(0.001))(DES1)
 ES2 = ELU()(S2)
-S3 = Dense(32,W_regularizer=l2(0.001))(S2)
+S3 = Dense(32,W_regularizer=l2(0.001))(ES2)
 ES3 = ELU()(S3)
 
-Steer = Dense(1, activation='linear',W_regularizer=l2(0.001))(ES3)
+Steer = Dense(1, activation='linear',W_regularizer=l2(0.001), name='steer_out')(ES3)
 
 G1 = Dense(32,W_regularizer=l2(0.001))(ED3)
 EG1 = ELU()(G1)
-G2 = Dense(32,W_regularizer=l2(0.001))(G1)
+DEG1 = Dropout(0.5)(EG1)
+G2 = Dense(32,W_regularizer=l2(0.001))(DEG1)
 EG2 = ELU()(G2)
-G3 = Dense(32,W_regularizer=l2(0.001))(G2)
+G3 = Dense(32,W_regularizer=l2(0.001))(EG2)
 EG3 = ELU()(G3)
 
-Accel = Dense(3, activation='sigmoid')(EG3)
+Accel = Dense(3, activation='sigmoid', name='gas_out')(EG3)
 
 model = Model(input=[real_in, frame_in], output=[Steer,Accel])
 
+sgd = SGD(lr=0.001)
 adam = Adam(lr=0.001)
 
-model.compile(loss=['mean_squared_error',
+model.compile(loss=['mse',
                     'categorical_crossentropy'],
-              optimizer=adam,
+              optimizer=sgd,
               metrics=['mse','accuracy'])
 
 # Switch to the compressed data input
@@ -97,14 +103,14 @@ imgs = np.load('data/imgs_arr_big.npz')['arr_0']
 speedx = np.load('data/speedx_arr_big.npz')['arr_0']
 targets = np.load('data/targets_arr_big.npz')['arr_0']
 gas_target = to_categorical(targets[:,1])
-nb_epoch = 10000
+nb_epoch = 40
 mini_epoch = 10
 num_steps = int(nb_epoch/mini_epoch)
 for step in tqdm(range(num_steps)):
-    h = model.fit([speedx, imgs], [targets[:,0],gas_target],
+    h = model.fit([speedx, imgs], {'steer_out':targets[:,0],'gas_out':gas_target},
                     batch_size = 32, nb_epoch=mini_epoch, verbose=1,
                     validation_split=0.1, shuffle=True)
-    model.save_weights('steer_simple_l2_big_{0}_{1:4.5}.h5'.format(step,h.history['loss'][-1]),overwrite=True)
+    model.save_weights('steer_simple_l2_big_{0}_{1:4.5}_{2:4.5}.h5'.format(step,h.history['val_steer_out_loss'][-1],h.history['val_gas_out_acc'][-1]),overwrite=True)
 
 model.save_weights('steer_simple_l2_big_final.h5',overwrite=True)
 model.load_weights('steer_simple_l2_big_final.h5')
@@ -145,13 +151,32 @@ def get_point(s,start=0,end=63,height= 16):
         X = end
     return (X,height)
 
+
+# evaluate the model at each point
+mse = []
+acc = []
+weights = sorted(glob.glob('*_0.*.h5'),
+        key = lambda x: int(x.split('_')[4]) )
+val_idx = (len(imgs)//10) * 9
+for wfile in tqdm(weights):
+    model.load_weights(wfile)
+    preds = model.predict([speedx[val_idx:],imgs[val_idx:]])
+    steer_preds = preds[0].reshape([-1])
+    gas_preds = np.argmax(preds[1],axis=1)
+    mse.append(metrics.mean_squared_error(targets[val_idx:,0],steer_preds))
+    acc.append(metrics.accuracy_score(targets[val_idx:,1],gas_preds))
+
+
+plt.plot(mse)
+plt.plot(acc)
+
 import matplotlib.animation as animation
 figure = plt.figure()
 imageplot = plt.imshow(np.zeros((64, 64, 3), dtype=np.uint8))
-def next_frame(i):
-    im = Image.fromarray(np.array(imgs[i].transpose(1,2,0),dtype=np.uint8))
+def next_frame(i,val_idx = 0):
+    im = Image.fromarray(np.array(imgs[val_idx+i].transpose(1,2,0),dtype=np.uint8))
     p = get_point(1-steer_preds[i])
-    t = get_point(1-targets[i,0])
+    t = get_point(1-targets[i+val_idx,0])
     draw = ImageDraw.Draw(im) 
     draw.line((32,63, p,p),
                 fill=(255,0,0,128))
@@ -164,4 +189,4 @@ def next_frame(i):
 animate = animation.FuncAnimation(figure, next_frame, frames=range(0,len(imgs)), interval=25, blit=False)
 plt.show()
 
-
+plt.plot(np.array([steer_preds.reshape(len(steer_preds)),targets[val_idx:,0]]).T,'.')
