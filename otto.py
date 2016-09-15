0fd4c0a3db14b0eaf1dfbc879a8c4ef4ea5c45b2
Dan Van Boxel <the.dvb@gmail.com>
2016-09-14 21:33:09 -0400

    Scaled with video!

--- otto.py
+++ otto.py
@@ -21,7 +21,9 @@ from keras import backend as K
 import sklearn.metrics as metrics
 
 import datetime
+import time
 
+from PIL import Image, ImageDraw
 import pygame
 import pygame.camera
 from pygame.locals import *
@@ -29,8 +31,10 @@ pygame.init()
 pygame.camera.init()
 
 debug = True
+video = True
 
 # setup model
+print("setting up model")
 ndata = 0
 imgsize = 64
 # frame size
@@ -90,11 +94,13 @@ model.compile(loss=['mse'],
 model.load_weights('/home/ubuntu/proj/autonomous/steer_only_current.h5')
 
 # initialize webcam
+print('initialize webcame')
 cams = pygame.camera.list_cameras()
 cam = pygame.camera.Camera(cams[0],(64,64),'RGB')
 cam.start()
 
 # make serial connection
+print('connect to serial port')
 if not debug:
     ser = serial.Serial('/dev/tty.usbmodem1411')
 else:
@@ -105,6 +111,7 @@ speeds = np.zeros(3,dtype=np.float32)
 
 # Start the loop 
 start = datetime.datetime.now()
+t = 0
 
 # function for output string
 def drive_str(steer, direction=1, speed=255, ms=0):
@@ -116,49 +123,96 @@ def drive_str(steer, direction=1, speed=255, ms=0):
     '''
     return '{0},{1},{2},{3}'.format(int(steer),int(direction),int(speed),int(ms))
 
-def do_loop():
+def get_point(s,start=0,end=63,height= 16):
+    ''' Figure out the other point for animation'''
+    X = int(s*(end-start))
+    if X < start:
+        X = start
+    if X > end:
+        X = end
+    return (X,height)
+
+if video == True:
+    print('setup animation')
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+    #plt.ion()
+    figure = plt.figure()
+    imageplot = plt.imshow(np.zeros((64, 64, 3), dtype=np.uint8))
+    from itertools import cycle
+    loop = cycle(range(10))
+else:
+    imageplot = False
+
+def do_loop(i=0):
     global speeds
+    global t
     # get image as numpy array
     img = pygame.surfarray.array3d(cam.get_image())
     # throw away non-square sides (left and rightmost 20 cols)
     img = img[20:-20]
     # Shrink to 64x64
-    img = scipy.misc.imresize(img,(64,64),'cubic','RGB').transpose(2,0,1)
-    # Read acceleration information (and time, TODO)
-    d = ser.readlines()
-    # most recent line
-    line = d[-1].strip()
+    img = scipy.misc.imresize(img,(64,64),'cubic','RGB').transpose(2,1,0)
+    if not debug:
+        # Read acceleration information (and time, TODO)
+        d = ser.readlines()
+        # most recent line
+        line = d[-1].strip()
+    else:
+        d = ser.readline()
+        line = d.strip()
     # parse into list
     data = list(map(float,line.split(',')))
     # save some info
     print('Saw {0}'.format(data), end='')
+    # get time in ms
+    now = datetime.datetime.now()
+    t_old = t
+    t = int((now-start).total_seconds()*1000)
     accel = np.array(data[:3],dtype=np.float32)
     accel[2] -= 1 # subtract accel due to gravity, maybe the car can fly :p
-    # update speeds, assume one "time unit" or multiply by elapsed ms
-    speeds = speeds + accel
+    # rescale inputs ( decide on max speed and accel of vehicle), clamp values to these
+    accel = accel / 10.
+    # update speeds, accel is scaled to be m/s**2 at this point
+    # so just multiply by seconds elapsed
+    speeds = speeds + accel*(t-t_old)/1000.
+    # now shift the accel
+    accel += 0.5
     # compute magnitude of speed and accel
     mspeed = np.sqrt(np.sum(speeds*speeds))
     maccel = np.sqrt(np.sum(accel*accel))
-    # rescale inputs ( decide on max speed and accel of vehicle), clamp values to these
-    # TODO
     # make prediction
     pred = model.predict([np.array([[mspeed,maccel]]),np.array([img])])
     # clamp values
     pred[0] = np.max([np.min([pred[0],1.0]),0.])
     # rescale output steering
     steer_p = int(255-255*pred[0])
-    # get time in ms
-    now = datetime.datetime.now()
-    t = int((now-start).total_seconds()*1000)
     # create str
     s = drive_str(steer_p,ms=t)
     print(' send {0}'.format(s))
     if not debug:
         ser.write(s)
+    if video == True:
+        im = Image.fromarray(np.array(img.transpose(1,2,0),dtype=np.uint8))
+        p = get_point(1-pred[0])
+        draw = ImageDraw.Draw(im) 
+        draw.line((32,63, p,p),
+                    fill=(255,0,0,128))
+        imageplot.set_array(im)
+    #if i % 10 == 0:
+    #    print(i)
+    return imageplot,
 
 
-while True:
-    do_loop()
+print('big rocket go now')
+if video == True:
+    animate = animation.FuncAnimation(figure, do_loop, frames=loop, interval=25, blit=False)
+    print('started animation')
+    plt.show()
+    print('should show animation')
+else:
+    while True:
+        do_loop()
 # cleanup
 ser.close()
 cam.stop()
