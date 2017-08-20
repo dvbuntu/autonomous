4709a856e01a07eeea1373362c7c17971303b827
ricklon <rick.rickanderson@gmail.com>
2017-08-20 15:38:46 -0400

    updated playdata for images

--- playdata.py
+++ playdata.py
@@ -1,22 +1,45 @@
+# Play training files
+
+import os
 import numpy as np
+import glob
 import cv2
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import argparse
+import time
 
+print("Finding data files")
 
-parser = argparse.ArgumentParser(description='Play the collected data.')
-parser.add_argument('-f','--data_dir', action='store', default='data')
-
+parser = argparse.ArgumentParser(description='Play Otto collected data')
+parser.add_argument('-f','--data-dir', action='store', default='data')
 args = parser.parse_args()
+data_dir = args.data_dir
 
+img_files = glob.glob(os.path.join(data_dir,'imgs*.npz'))
+speedx_files = glob.glob(os.path.join(data_dir,'speedx*.npz'))
+target_files = glob.glob(os.path.join(data_dir,'targets*.npz'))
 
-# last 10% of every file provides validation...
-img_files = glob.glob(os.path.join([data_dir,'imgs*.npz']))
-speedx_files = glob.glob(os.path.join([data_dir,'speedx*.npz']))
-target_files = glob.glob(os.path.join([data_dir,'targets*.npz']))
+plt.ion()
 
-for i,s,t in zip(img_files, speedx_files, target_files):
+for i,s,t in zip(sorted(img_files), sorted(speedx_files), sorted(target_files)):
+  print(i, s, t)
   imgs = np.load(i)['arr_0']
   speedx = np.load(s)['arr_0']
   targets = np.load(t)['arr_0']
+
+  
+  print('imgs: ', imgs.shape)
+  # print(imgs)
+  for im in imgs:
+    print(im.shape)
+    print(im)
+    # cv2.imshow('im', im)
+    plt.imshow(im.transpose(1,2,0))
+
+# cv2.destroyAllWindows()
+
+
+
 
 
