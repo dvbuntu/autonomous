42c18259bd41c9f759615bc2b11b82c2afc7db01
ricklon <rick.rickanderson@gmail.com>
2017-11-05 16:51:00 -0500

    added i2c to new ottofullautodrive

--- arduino/NewOTTOFullAutoDrive/src/NewOTTOFullAutoDrive.ino
+++ arduino/NewOTTOFullAutoDrive/src/NewOTTOFullAutoDrive.ino
@@ -6,10 +6,31 @@
 */
 #include <Arduino.h>
 #include <SoftPWMServo.h>
-#include "MPU9250.h"
+#include <Wire.h>
 
 #define DEBUG_SERIAL false
 #define MAX_CMD_BUF  17
+
+
+/*
+  Define IMU mpu9250 values
+
+*/
+#define    MPU9250_ADDRESS            0x68
+#define    MAG_ADDRESS                0x0C
+
+#define    GYRO_FULL_SCALE_250_DPS    0x00  
+#define    GYRO_FULL_SCALE_500_DPS    0x08
+#define    GYRO_FULL_SCALE_1000_DPS   0x10
+#define    GYRO_FULL_SCALE_2000_DPS   0x18
+
+#define    ACC_FULL_SCALE_2_G        0x00  
+#define    ACC_FULL_SCALE_4_G        0x08
+#define    ACC_FULL_SCALE_8_G        0x10
+#define    ACC_FULL_SCALE_16_G       0x18
+
+#define WHO_AM_I_MPU9250 0x75 // Should return 0x71
+
 
 /*
    Setup RC Controller
@@ -61,9 +82,6 @@ const int PIN_STR = 10;
 int PREV_DIR = LOW;
 const int SHOOT_DELAY = 250;
 
-//imu unit object
-MPU9250 ottoIMU;
-
 int thrData[2] = {0, 0};
 
 int log_thr = 0;
@@ -73,46 +91,49 @@ unsigned long steer_history[10];
 int steer_next_ind;
 
 
-int initIMU() {
-  // Read the WHO_AM_I register, this is a good test of communication
-  byte addr = ottoIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
-  if (addr == 0x71) // WHO_AM_I should always be 0x68
-  {
-    if (DEBUG_SERIAL) {
-      Serial.println("MPU9250 is online...");
-    }
-    ottoIMU.MPU9250SelfTest(ottoIMU.SelfTest);
-    // Calibrate gyro and accelerometers, load biases in bias registers
-    ottoIMU.calibrateMPU9250(ottoIMU.gyroBias, ottoIMU.accelBias);
-    ottoIMU.initMPU9250();
-    // Initialize device for active mode read of acclerometer, gyroscope, and
-    // temperature
-    if (DEBUG_SERIAL) {
-      Serial.println("MPU9250 initialized for active data mode....");
-    }
-    // Read the WHO_AM_I register of the magnetometer, this is a good test of
-    // communication
-    byte maddr = ottoIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
-    if (DEBUG_SERIAL) {
-      Serial.printf("AK8963 I AM %02x  I should be %02x \n", maddr, 0x48);
-    }
-    // Get magnetometer calibration from AK8963 ROM
-    ottoIMU.initAK8963(ottoIMU.magCalibration);
-    // Initialize device for active mode read of magnetometer
-    if (DEBUG_SERIAL) {
-      Serial.println("AK8963 initialized for active data mode....");
-    }
-  }
+// This function read Nbytes bytes from I2C device at address Address. 
+// Put read bytes starting at register Register in the Data array. 
+void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.endTransmission();
+  
+  // Read Nbytes
+  Wire.requestFrom(Address, Nbytes); 
+  uint8_t index=0;
+  while (Wire.available())
+    Data[index++]=Wire.read();
+}
 
-  else
-  {
-    if (DEBUG_SERIAL) {
-      Serial.printf("Could not connect to MPU9250: 0x %02x\n", addr);
-    }
-    //while (1) ; // Loop forever if communication doesn't happen
-    return false;
-  }
-  return true;
+
+// Write a byte (Data) in device (Address) at register (Register)
+void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
+{
+  // Set register address
+  Wire.beginTransmission(Address);
+  Wire.write(Register);
+  Wire.write(Data);
+  Wire.endTransmission();
+}
+
+
+int initIMU() {
+  // Set accelerometers low pass filter at 5Hz
+  I2CwriteByte(MPU9250_ADDRESS,29,0x06);
+  // Set gyroscope low pass filter at 5Hz
+  I2CwriteByte(MPU9250_ADDRESS,26,0x06);
+ 
+  
+  // Configure gyroscope range
+  I2CwriteByte(MPU9250_ADDRESS,27,GYRO_FULL_SCALE_1000_DPS);
+  // Configure accelerometers range
+  I2CwriteByte(MPU9250_ADDRESS,28,ACC_FULL_SCALE_4_G);
+  // Set by pass mode for the magnetometers
+  I2CwriteByte(MPU9250_ADDRESS,0x37,0x02);
+  // Request continuous magnetometer measurements in 16 bits
+  I2CwriteByte(MAG_ADDRESS,0x0A,0x16);
 }
 
 void doAction() {
@@ -189,18 +210,35 @@ void doAction() {
         1600 - 1900: forward
     */
     setThrottle(ch[CH_THR]);
-    ottoIMU.readAccelData(ottoIMU.accelCount); // Read the x/y/z adc values
-    ottoIMU.getAres();
-    ottoIMU.ax = (float)ottoIMU.accelCount[0] * ottoIMU.aRes; // - accelBias[0];
-    ottoIMU.ay = (float)ottoIMU.accelCount[1] * ottoIMU.aRes; //   accelBias[1];
-    ottoIMU.az = (float)ottoIMU.accelCount[2] * ottoIMU.aRes; // - accelBias[2];
-    ottoIMU.readGyroData(ottoIMU.gyroCount); // Read the x/y/z adc values
-    ottoIMU.getGres();
-    ottoIMU.gx = (float)ottoIMU.gyroCount[0] * ottoIMU.gRes;
-    ottoIMU.gy = (float)ottoIMU.gyroCount[1] * ottoIMU.gRes;
-    ottoIMU.gz = (float)ottoIMU.gyroCount[2] * ottoIMU.gRes;
-    printData(ottoIMU.ax, ottoIMU.ay, ottoIMU.az, ottoIMU.gx, ottoIMU.gy,
-              ottoIMU.gz, millis(), ch[CH_STR], ch[CH_THR]);
+    // Read accelerometer and gyroscope
+    uint8_t Buf[14];
+    I2Cread(MPU9250_ADDRESS,0x3B,14,Buf);
+  
+    // Create 16 bits values from 8 bits data
+    // Accelerometer
+    int16_t ax=-(Buf[0]<<8 | Buf[1]);
+    int16_t ay=-(Buf[2]<<8 | Buf[3]);
+    int16_t az=Buf[4]<<8 | Buf[5];
+
+    // Gyroscope
+    int16_t gx=-(Buf[8]<<8 | Buf[9]);
+    int16_t gy=-(Buf[10]<<8 | Buf[11]);
+    int16_t gz=Buf[12]<<8 | Buf[13];
+    // _____________________
+    // :::  Magnetometer ::: 
+    // Read register Status 1 and wait for the DRDY: Data Ready
+    // I2Cread(MAG_ADDRESS,0x02,1,&ST1);
+    // Read magnetometer data  
+    //uint8_t Mag[7];  
+    //I2Cread(MAG_ADDRESS,0x03,7,Mag);    
+    // Create 16 bits values from 8 bits data 
+    // Magnetometer
+    //int16_t mx=-(Mag[3]<<8 | Mag[2]);
+    //int16_t my=-(Mag[1]<<8 | Mag[0]);
+    //int16_t mz=-(Mag[5]<<8 | Mag[4]);
+
+    printData(ax, ay, az, gx, gy,
+              gz, millis(), ch[CH_STR], ch[CH_THR]);
   }
 }
 
