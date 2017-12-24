037697d92aa9f0edc69ed1d7ec8a6a283feea230
digsrus <jeo@jims-Mac-mini.local>
2017-12-23 22:10:19 -0500

    more fixation

--- services/switch_button_test.py
+++ services/switch_button_test.py
@@ -15,25 +15,25 @@ button_run_autonomous_pin = 22
 switch_shutdown_RPi_pin = 10
 switch_collect_data_pin = 9
 
+# LEDpowerStatusOfRPi = hardwired to +3.3 volt pin #1 of RPi
+LED_collect_data_pin = 5
+LED_shutdown_RPi_pin = 6
+
 GPIO.setmode( GPIO.BCM )  
 GPIO.setwarnings( False )  
 
 #  falling edge detection setup for all buttons and switches
-GPIO.setup( button_copy_to_SDcard, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
-GPIO.setup( button_run_autonomous, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
-GPIO.setup( button_read_from_SDcard, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
-GPIO.setup( switch_shutdown_RPi, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
-GPIO.setup( switch_collect_data, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
+GPIO.setup( button_copy_to_SDcard_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
+GPIO.setup( button_run_autonomous_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
+GPIO.setup( button_read_from_SDcard_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
+GPIO.setup( switch_shutdown_RPi_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
+GPIO.setup( switch_collect_data_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP ) 
 
-GPIO.setup( LED_copy_to_SDcard, GPIO.OUT )
-GPIO.setup( LED_read_from_SDcard, GPIO.OUT )
-GPIO.setup( LED_autonomous, GPIO.OUT )
-GPIO.setup( LED_shutdown_RPi, GPIO.OUT )
-GPIO.setup( LED_collect_data, GPIO.OUT )
-
-# LEDpowerStatusOfRPi = hardwired to +3.3 volt pin #1 of RPi
-LED_collect_data_pin = 5
-LED_shutdown_RPi_pin = 6
+GPIO.setup( LED_copy_to_SDcard_pin, GPIO.OUT )
+GPIO.setup( LED_read_from_SDcard_pin, GPIO.OUT )
+GPIO.setup( LED_autonomous_pin, GPIO.OUT )
+GPIO.setup( LED_shutdown_RPi_pin, GPIO.OUT )
+GPIO.setup( LED_collect_data_pin, GPIO.OUT )
 
 def callback_button_copy_to_SDcard( channel ): 
 	pass
