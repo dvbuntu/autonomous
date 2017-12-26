43a4ffa4a6231c4b7a6d1bf33db8ee5d5ae2deb4
digsrus <jeo@jims-Mac-mini.local>
2017-12-26 15:23:43 -0500

    move edits

--- services/ottoUSBstickFunctions.py
+++ services/ottoUSBstickFunctions.py
@@ -2,9 +2,9 @@
 
 #------------- Functions for handling reads and writes from USB stick ---------------
 
-def read_USB_stick:
+def read_USB_stick():
 	print( "usb stick read function" )
 	
-def write_USB_stick:
+def write_USB_stick():
 	print( "usb stick write function" )
 	
