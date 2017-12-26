2726a2059d52290b032d5fb3ee66c14e8d571a9a
digsrus <jeo@jims-Mac-mini.local>
2017-12-26 17:20:33 -0500

    move edits

---
+++ services/ottoUSBdriveFunctions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#------------- Functions for handling reads and writes from USB drive ---------------
+
+def read_USB_drive():
+	print( "usb drive read function" )
+	
+def write_USB_drive():
+	print( "usb drive write function" )
+	
+# 
